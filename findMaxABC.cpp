184de33b2cfa48cc9317d0a9ab56efd10be00250
yaqian <Yaqian.Lu@cxmt.com>
2024-12-30 19:04:08 +0800

    Update findMaxABC.cpp

--- findMaxABC.cpp
+++ findMaxABC.cpp
@@ -7,7 +7,7 @@ void findMaxABC(int N) {
     int maxA = 0, maxB = 0, maxC = 0;
     bool found = false;
     for (int C = 0; C <= N; ++C) {
-        if ((N - C) % 7 == 0) {
+        if ((N - C) % 7 == 0) {   //why  
             int AB = N - C;
             for (int B = 0; B <= AB; ++B) {
                 int A = AB - B;
